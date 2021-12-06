69553c2d6151083b9e14042a1b06583430b08baf
Pahlevi Pasha <pahlevi.pasha09@gmail.com>
2021-12-06 21:03:48 +0700

    Add project files.

---
+++ ServiceReservasi/Service1Base.cs
@@ -0,0 +1,25 @@
+﻿namespace ServiceReservasi
+{
+    public class Service1Base
+    {
+
+        public string pemesanan(string IDPemesanan, string NamaCustomer, string NoTelpon, int JumlahPemesanan, string IDLokasi)
+        {
+            string a = "gagal";
+            try
+            {
+                string sql = "insert into dbo.Pemesanan values ('" + IDPemesanan + "', '" + NamaCustomer + "', '" + NoTelpon + "', '" + JumlahPemesanan + "', '" + JumlahPemesanan + "', '" + IDLokasi + "')";
+                connection = new SqlConnection(constring);
+                com = new SqlCommand(sql, connection);
+                connection.Open();
+                com.ExecuteNonQuery();
+                connection.Close();
+                a = "sukses";
+            }
+            catch (Exception es)
+            {
+                Console.WriteLine(es);
+            }
+        }
+    }
+}
